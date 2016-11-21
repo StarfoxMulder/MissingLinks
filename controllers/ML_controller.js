34a8e4f9fa3235e5af79f01ccaafb04bfd2ef5ae
Mary Miller <marymiller1954@gmail.com>
2016-11-21 07:56:36 -0500

    Adding ML-Controller in case it is needed

--- controllers/ML_controller.js
+++ controllers/ML_controller.js
@@ -1,165 +1,92 @@
+// Our Missing Links controller
+// =====================
+
+// This file uses Sequelize to manage data manipulation
+// for all apropos http requests.
+
+
 var express = require('express');
 var router = express.Router();
-var burger = require('../models/burger.js');
-var connection = require('../config/connection.js');
-var models = require('../models');
-var multer = require('multer');
+// edit available and wanted models to match sequelize
+var available = require('../models/')["available"];
+var wanted = require('../models/')["wanted"];
 
-///// Route to Landing Page \\\\\
-router.get('/', function (req, res) {
-  res.redirect('/landing');
-  //need a landing.handebars
+//get route -> index
+router.get('/', function(req,res) {
+    // send us to the next get function instead.
+    res.redirect('/index')
 });
 
 
-///// Route to Available Items List Page \\\\\
-router.get('/listA', function (req, res) {
-  models.available.findAll({})
-  .then(function(data) {
-    res.render('listA', { available : data});
-    //this returns all available items
-    // need model for available items
-  });
+
+
+// get route, edited to match sequelize
+router.get('/available', function(req,res) {
+    // replace old function with sequelize function
+    available.findAll({include:{model: available}})
+    // use promise method to pass the available items...
+    .then(function(available_data){
+        console.log(available_data);
+        // into the main index, updating the page
+        return res.render('index', {available_data})
+    })
 });
 
-///// Route to Wanted Items List Page \\\\\
-router.get('/listW', function (req, res) {
-  models.wanted.findAll({})
-  .then(function(data) {
-    res.render('listW', { wanted : data});
-    //this returns all wanted items
-    // need model for wanted items
-  });
-});
-
-
-///// Route from /post/insertOne to /list \\\\\
-router.post('/post/available', function (req, res){
-
-  models.available.create({
-    price: req.body.price,
-    itemName: req.body.itemName,
-    posterName: req.body.posterName,
-    longDescription: req.body.longDescription,
-    shortDescription: req.body.shortDescription,
-    sold: false
-    // mainPic: /*UPDATE W/MULTER PARAMS*/,
-    // pic2: /*UPDATE W/MULTER PARAMS*/,
-    // pic3: /*UPDATE W/MULTER PARAMS*/,
-    // pic4: /*UPDATE W/MULTER PARAMS*/,
-  })
-  .then(function() {
-    res.redirect('/listA');
-    //Should we do some kind of 'post successful' redirect here
-    // as an intermediary page between /post and redirect back to /list?
-    // Or do another form of validation?
-  });
-
-});
-
-///// Route from /post/wanted to /list \\\\\
-router.post('/post/wanted', function (req, res){
-
-  models.wanted.create({
-    price: req.body.price,
-    itemName: req.body.itemName,
-    posterName: req.body.posterName,
-    longDescription: req.body.longDescription,
-    shortDescription: req.body.shortDescription,
-    found: false
-    // mainPic: /*UPDATE W/MULTER PARAMS*/,
-    // pic2: /*UPDATE W/MULTER PARAMS*/,
-    // pic3: /*UPDATE W/MULTER PARAMS*/,
-    // pic4: /*UPDATE W/MULTER PARAMS*/,
-  })
-  .then(function() {
-    res.redirect('/listW');
-    //Should we do some kind of 'post successful' redirect here
-    // as an intermediary page between /post and redirect back to /list?
-    // Or do another form of validation?
-  });
-
+// get route, edited to match sequelize
+router.get('/wanted', function(req,res) {
+    // replace old function with sequelize function
+    wanted.findAll({include:{model: wanted}})
+    // use promise method to pass the available items...
+    .then(function(wanted_data){
+        console.log(wanted_data);
+        // into the main index, updating the page
+        return res.render('index', {wanted_data})
+    })
 });
 
 
-///// router for getting info of a specific Available Item for ItemA Page \\\\\
-router.get('/itemA/:id', function(req, res) {
-  var id = req.params.id;
-  var condition = 'id = '+id;
 
-  models.available.findById(id)
-  .then(function(data){
-    res.render('itemA', {available : data});
-  });
 
+
+
+
+// post route to add available items
+router.post('/available/create', function(req, res) {
+    // create a new item
+    available.create({price: req.body.available_price mainPic: req.body.available_mainPic  pic2: req.body.available_pic2
+    pic3: req.body.available_pic3 pic4: req.body.available_pic4 itemName: req.body.available_itemName 
+    posterName: req.body.available_posterName  posterContactNumber: req.body.available_posterContactNumber
+    longDescription: req.body.available_longDescription   sold: req.body.available_sold datePosted: req.body.available_datePosted
+    })
+
+    // pass the result of our call
+    .then(function(newAvailable ){
+        // log the result to our terminal/bash window
+        console.log(newAvailable);
+        // redirect
+        res.redirect('/');
+    });
+});
+
+// post route to add wanted items
+router.post('/available/create', function(req, res) {
+    // create a new item
+    wanted.create({price: req.body.wanted_price mainPic: req.body.wanted_mainPic  pic2: req.body.wanted_pic2
+    pic3: req.body.wanted_pic3 pic4: req.body.wanted_pic4 itemName: req.body.wanted_itemName 
+    posterName: req.body.wanted_posterName  posterContactNumber: req.body.wanted_posterContactNumber
+    longDescription: req.body.wanted_longDescription   sold: req.body.wanted_sold datePosted: req.body.wanted_datePosted
+    })
+
+    // pass the result of our call
+    .then(function(newWanted){
+        // log the result to our terminal/bash window
+        console.log(newWanted);
+        // redirect
+        res.redirect('/');
+    });
 });
 
 
-///// route from item page to update 'sold' field when purchased \\\\\
-router.put('/itemA/sold/:id', function (req, res) {
-  var condition = 'id = ' + req.params.id;
-
-  models.available.updateSold({
-    sold: true
-  },
-  {
-    where: {
-      id:condition
-    }
-  }).then(function () {
-    res.redirect('/listA');
-    //Do we want an intermediary validation page here so users
-    // know that their purchase was successful?
-    // Or another type of validation?
-  });
-});
-
-///// router for getting info of a specific Wanted Item for ItemW Page \\\\\
-router.get('/itemW/:id', function(req, res) {
-  var id = req.params.id;
-  var condition = 'id = '+id;
-
-  models.wanted.findById(id)
-  .then(function(data){
-    res.render('itemW', {wanted : data});
-  });
-
-});
-
-
-///// route from item page to update 'found' field when an item is found - i think in theory this would lead to connecting the two users so they can coordinate on how to complete the transaction independently \\\\\
-router.put('/itemW/found/:id', function (req, res) {
-  var condition = 'id = ' + req.params.id;
-
-  models.wanted.updateFound({
-    found: true
-  },
-  {
-    where: {
-      id:condition
-    }
-  }).then(function () {
-    res.redirect('/listW');
-    //Do we want an intermediary validation page here so users
-    // know that their purchase was successful?
-    // Or another type of validation?
-  });
-});
-
-
-///// in case we need a delete/remove item option \\\\\
-// router.delete('/item/deleteOne/:id', function (req, res) {
-//     //sequelize update
-//   var condition = 'id = ' + req.params.id;
-
-//   item.deleteOne({
-//     where: {
-//       id:condition
-//     }
-//   }).then(function () {
-//     res.redirect('/list');
-//   });
-// });
 
 
 module.exports = router;
