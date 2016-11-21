8f72ac15632922de9bbd596238e7dd40b029efe4
Charles Prestia <cjprestia@gmail.com>
2016-11-21 11:01:22 -0500

    adding missing semicolons

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,20 +6,20 @@ INSERT INTO available (`price`, `mainPic`, `itemName`, `posterName`, `longDescri
 
 INSERT INTO wanted (`mainPic`, `itemName`, `posterName`, `longDescription`, `shortDescription`, `found`)
  VALUES ("https://s-media-cache-ak0.pinimg.com/564x/87/9c/34/879c3497d7f2adc128b29ee2df404820.jpg",
-  "One 4 Diamonds Drop Earring", "Maria Doyle", "919-111-1234", "14 Karat Yellow Gold post drop earring", "0")
+  "One 4 Diamonds Drop Earring", "Maria Doyle", "919-111-1234", "14 Karat Yellow Gold post drop earring", "0");
 
 INSERT INTO available (`price`, `mainPic`, `itemName`, `posterName`, `longDescription`, `shortDescription`)
- VALUES ("125.00", "http://thumbs.ebaystatic.com/images/g/Cr0AAOSwKOJYJRdk/s-l225.jpg", 
+ VALUES ("125.00", "http://thumbs.ebaystatic.com/images/g/Cr0AAOSwKOJYJRdk/s-l225.jpg",
  	"Cup", "Maria Doyle", "919-111-1234", "One Heart-Shaped Coffee Cup", "0");
 
 INSERT INTO wanted (`mainPic`, `itemName`, `posterName`, `longDescription`, `shortDescription`, `found`)
- VALUES ("http://thumbs.ebaystatic.com/images/g/Cr0AAOSwKOJYJRdk/s-l225.jpg", 
- 	"One Heart-Shaped Coffee Cup", "Mary Miller", "919-451-0966", "14 Karat Yellow Gold post rop earring", "0")
+ VALUES ("http://thumbs.ebaystatic.com/images/g/Cr0AAOSwKOJYJRdk/s-l225.jpg",
+ 	"One Heart-Shaped Coffee Cup", "Mary Miller", "919-451-0966", "14 Karat Yellow Gold post rop earring", "0");
 
 INSERT INTO available (`mainPic`, `itemName`, `posterName`, `longDescription`, `shortDescription`, `found`)
  VALUES ("200.00", "http://thumbs.ebaystatic.com/images/m/mesJPJ0Vbtt5X7F3t3E4dOA/s-l225.jpg",
-  "Hiking Shoe", "Mary Miller", "919-451-6666", "Leather Hiking Shoe Men's size 10", "0")
+  "Hiking Shoe", "Mary Miller", "919-451-6666", "Leather Hiking Shoe Men's size 10", "0");
 
 INSERT INTO wanted (`mainPic`, `itemName`, `posterName`, `longDescription`, `shortDescription`, `found`)
- VALUES ("http://thumbs.ebaystatic.com/images/m/mesJPJ0Vbtt5X7F3t3E4dOA/s-l225.jpg", 
- 	"Hiking Shoe", "Charles J. Prestia", "919-451-0966", "Leather Hiking Shoe Men's size 10", "0")
+ VALUES ("http://thumbs.ebaystatic.com/images/m/mesJPJ0Vbtt5X7F3t3E4dOA/s-l225.jpg",
+ 	"Hiking Shoe", "Charles J. Prestia", "919-451-0966", "Leather Hiking Shoe Men's size 10", "0");
